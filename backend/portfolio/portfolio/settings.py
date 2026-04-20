69705a06d2fc7f65f8177f35c1861bde36bc6da0
Endalkachew <endalkyz@gmail.com>
2026-04-20 08:50:35 +0300

    last

--- backend/portfolio/portfolio/settings.py
+++ backend/portfolio/portfolio/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key')
 DEBUG = True
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-ALLOWED_HOSTS = ['endalkachew.pythonanywhere.com']
+ALLOWED_HOSTS = ['endalkachew.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 # Installed apps
 INSTALLED_APPS = [
